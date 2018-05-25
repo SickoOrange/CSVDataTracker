f67638b3f1a45c71258e34be9e9ab4cd29f271ea
Ya Yin <ya.yin@evosoft.com>
2018-05-25 16:48:12 +0200

    fix(slidemenu): add button selected color

--- src/main/java/com/dls/aa/controller/SlideMenuController.java
+++ src/main/java/com/dls/aa/controller/SlideMenuController.java
@@ -1,7 +1,10 @@
 package com.dls.aa.controller;
 
 import com.dls.aa.ServiceContainer;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXButton.ButtonType;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -37,7 +40,7 @@ public class SlideMenuController implements Initializable {
   private JFXButton chainPathBtn;
 
   @FXML
-  private JFXButton naviToAfiAndConnection;
+  private JFXButton trendsBtn;
 
   @FXML
   private JFXButton moduleStructureBtn;
@@ -47,6 +50,9 @@ public class SlideMenuController implements Initializable {
 
   @FXML
   private JFXButton hamsterBtn;
+  private ImmutableList<JFXButton> btnList;
+  private String styleTextPressedValue = "-fx-text-fill: #E33461";
+  private String styleTextReleasedValue = "-fx-text-fill: #98A0A6";
 
 
   private void setNode(Node node) {
@@ -59,21 +65,34 @@ public class SlideMenuController implements Initializable {
   }
 
 
+  private void setButtonFocus(ActionEvent event) {
+    long count = btnList.stream()
+        .peek(b -> b.setStyle(styleTextReleasedValue)).peek(b -> {
+          if (b == event.getSource()) {
+            b.setStyle(styleTextPressedValue);
+          }
+        }).count();
+  }
+
+
   @FXML
   void onAfiTypeBtnClick(ActionEvent event) {
     System.out.println("start AfiType Pane");
+    setButtonFocus(event);
     setNode(getNodeMapping().get(AfiTypeController.class.getSimpleName()));
   }
 
   @FXML
   void onAfiBtnClick(ActionEvent event) {
     System.out.println("Start Afi Pane");
+    setButtonFocus(event);
     setNode(getNodeMapping().get(AfiController.class.getSimpleName()));
   }
 
   @FXML
   void onPortsBtnClick(ActionEvent event) {
     System.out.println("Start Ports Pane");
+    setButtonFocus(event);
     setNode(getNodeMapping().get(PortsController.class.getSimpleName()));
 
   }
@@ -82,6 +101,7 @@ public class SlideMenuController implements Initializable {
   @FXML
   void onConnectionsBtnClick(ActionEvent event) {
     System.out.println("start connections Pane");
+    setButtonFocus(event);
     setNode(getNodeMapping().get(ConnectionsController.class.getSimpleName()));
 
   }
@@ -89,36 +109,45 @@ public class SlideMenuController implements Initializable {
   @FXML
   void onChainPathBtnClick(ActionEvent event) {
     System.out.println("start chainVisualizationPane");
+    setButtonFocus(event);
     setNode(getNodeMapping().get(ChainController.class.getSimpleName()));
   }
 
   @FXML
   void onModuleStructureBtnClick(ActionEvent event) {
     System.out.println("start moduleStructurePane");
+    setButtonFocus(event);
     setNode(getNodeMapping().get(ModuleStructureController.class.getSimpleName()));
   }
 
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    homeBtn.setStyle(styleTextPressedValue);
+    btnList = ImmutableList.<JFXButton>builder()
+        .add(homeBtn, afiBtn, afiTypeBtn, portsBtn, connectionsBtn, moduleStructureBtn,
+            chainPathBtn, hamsterBtn, trendsBtn).build();
 
   }
 
   @FXML
-  public void onHomeBtnClick(ActionEvent actionEvent) {
+  public void onHomeBtnClick(ActionEvent event) {
     System.out.println("Home");
+    setButtonFocus(event);
     setNode(getNodeMapping().get(DefautContentController.class.getSimpleName()));
   }
 
   @FXML
   void onTrendsBtnClick(ActionEvent event) {
     System.out.println("Trends Analysis Pane");
+    setButtonFocus(event);
     setNode(getNodeMapping().get(TrendsAnalysisController.class.getSimpleName()));
   }
 
   @FXML
   void onHamsterBtnClick(ActionEvent event) {
     System.out.println("Hamster Pane");
+    setButtonFocus(event);
     setNode(getNodeMapping().get(HamsterController.class.getSimpleName()));
   }
 }
